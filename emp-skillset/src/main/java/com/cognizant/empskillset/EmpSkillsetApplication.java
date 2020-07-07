57d0f6debc639478082967a3a85fda99afabbb84
Urvi Parekh <urvipparekh94@gmail.com>
2020-07-07 15:31:48 +0530

    adding test method

--- emp-skillset/src/main/java/com/cognizant/empskillset/EmpSkillsetApplication.java
+++ emp-skillset/src/main/java/com/cognizant/empskillset/EmpSkillsetApplication.java
@@ -13,5 +13,10 @@ public class EmpSkillsetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmpSkillsetApplication.class, args);
 	}
+	
+	public void test()
+	{
+		
+	}
 
 }
